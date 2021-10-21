9a347847fce4a43adf3fd8e615c7e396ada938b5
Anoop Chaudhary <anoopmonu02@gmail.com>
2021-10-21 17:35:33 +0530

    Training stage Done

--- src/training.py
+++ src/training.py
@@ -1,12 +1,25 @@
 from utils.common import read_config
 from utils.data_mgmt import getData
+from utils.model import create_model
 import argparse
 
 def training(config_path):
     config = read_config(config_path)
     validation_datasize = config["params"]["validation_datasize"]
     (X_train, y_train), (X_valid, y_valid),(X_test, y_test) = getData(validation_datasize)
-    print(config)
+
+    LOSS_FUNCTION = config["params"]["loss_function"]
+    OPTIMIZER = config["params"]["optimizer"]
+    METRICS = config["params"]["metrices"]
+    NUM_CLASSES = config["params"]["num_classes"]
+    
+    model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES)
+
+    EPOCHS = config["params"]["epochs"]
+    VALIDATION = (X_valid, y_valid)
+
+    history = model.fit(X_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
+    
  
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
