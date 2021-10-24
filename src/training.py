8789e59ac92af86eec977e0e2bca79e94ae359fb
Anoop Chaudhary <anoopmonu02@gmail.com>
2021-10-24 21:41:11 +0530

    Added a save model function

--- src/training.py
+++ src/training.py
@@ -1,7 +1,8 @@
 from utils.common import read_config
 from utils.data_mgmt import getData
-from utils.model import create_model
+from utils.model import create_model, save_model
 import argparse
+import os
 
 def training(config_path):
     config = read_config(config_path)
@@ -19,7 +20,16 @@ def training(config_path):
     VALIDATION = (X_valid, y_valid)
 
     history = model.fit(X_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
-    
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_name = config["artifacts"]["model_name"]
+    model_dir = config["artifacts"]["model_dir"]
+
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    print(f"PATH:{model_dir_path}")
+    os.makedirs(model_dir_path, exist_ok=True)
+
+    save_model(model, model_name, model_dir_path)
+
  
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
--- src/utils/model.py
+++ src/utils/model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-
+import time
+import os
 
 def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
     LAYERS = [
@@ -15,7 +16,17 @@ def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
     OPTIMIZER = "SGD"
     METRICS = ["accuracy"]
 
-
     model_clf.compile(loss=LOSS_FUNCTION, optimizer=OPTIMIZER, metrics=METRICS)
+    return model_clf  # << untrained Model
 
-    return model_clf ## << untrained Model
+
+def get_unique_filename(filename):
+    unique_filename = time.strftime(f"%Y%m%d_%H%M%S{filename}")
+    return unique_filename
+
+
+def save_model(model, model_name, model_dir):
+    unique_filename = get_unique_filename(model_name)
+    path_to_model = os.path.join(model_dir, unique_filename)
+    print(path_to_model)
+    model.save(path_to_model)
